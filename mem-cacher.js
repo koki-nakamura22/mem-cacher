ebe35d6e9ea91f465e249201374475debb2dce04
Koki Nakamura <nk.levia22@gmail.com>
2022-03-23 14:46:07 +0900

    refactoring

--- mem-cacher.js
+++ mem-cacher.js
@@ -9,20 +9,27 @@ function deepCopy(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
-function extractFunctionParameters(func) {
+/**
+ * Extract the function parameters names
+ * @param {Function} func
+ * @returns {Array} The function parameters names
+ */
+function extractFuncParamsNames(func) {
   const funcStr = func
     .toString()
     .replace(/\/\*[\s\S]*?\*\//g, "") // Remove comments of the form /* ... */
     .replace(/\/\/(.)*/g, "") // Removing comments of the form //
     .replace(/{[\s\S]*}/, "") // Remove body of the function { ... }
+    .replace(/^async /, "") // Remove a leading async keyword
     .replace(/=>/g, "") // removing '=>' if func is arrow function
     .trim();
 
   // Start parameter names after first '('
+  // If '(' does not find, the start position is 0
   const start = funcStr.indexOf("(") + 1;
 
-  // End parameter names is just before last ')'
-  const end = funcStr.length - 1;
+  // End parameter names is just before last ')' or end of string if ')' does not find
+  const end = funcStr.length - (funcStr.slice(-1) === ")" ? 1 : 0);
 
   const result = funcStr.substring(start, end).split(", ");
 
